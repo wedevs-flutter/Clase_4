e6f48e9bac3a10b48b8f50dff2c25fd859da22e5
Nicole Garcia <nicolepgc91@gmail.com>
2020-03-25 19:16:17 -0400

    implemetacion del boton inferior

--- lib/drawerm.dart
+++ lib/drawerm.dart
@@ -6,51 +6,115 @@ class Drawerm extends StatefulWidget {
 }
 
 class _DrawermState extends State<Drawerm> {
+ 
   @override
+   //posicion de las pnatallas
+  int _numerop = 0;
+  //creamos las combinaciones de pantalla donde usamos una lista de widgets
+  static const List<Widget> _pantallas = <Widget>[
+    Text(
+      'Posición 0: Juegos',style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'Posición 1: Películas',style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'Posición 2: Apps',style: TextStyle(fontSize: 40),
+    ),
+    Text(
+      'Posición 3: Libros',style: TextStyle(fontSize: 40),
+    ),
+    
+  ];
+  //funcion para recorrer las pantallas
+  void _recorrido(int index) {
+    setState(() {
+      _numerop = index;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: _pantadra(),
+      //BottomNavigatonBar= para navegar con las pantallas mediantes los botones
+      bottomNavigationBar: _botonave(),
+      // para el cambio de pantalla en el body cambiamos y colocamos la 
+      //funcion para acceder a las posiciones
+      body: Center(
+        child: _pantallas.elementAt(_numerop),
+      ),
+      //Drawer=Pantalla de cuentas que se desliza de izq a derecha
+      drawer: _drawern(),
       appBar: AppBar(
         title: Text("MANEJO DRAWER"),
         centerTitle: true,
-        //leading: Icon(Icons.person_add),
         actions: <Widget>[Icon(Icons.person_add)],
       ),
     );
   }
 
-  Widget _pantadra() {
+  Widget _botonave() {
+    return BottomNavigationBar(
+      //maenjamos los tipos de animacion
+      type: BottomNavigationBarType.fixed,
+      //cambiamos el color de los item (icono, texto)
+      unselectedItemColor: Colors.purple,
+      //color de cuando se pone un evento
+      fixedColor: Colors.teal,
+      //asignamos el numero de pantallas
+      currentIndex: _numerop,
+      //manejamos el recorrido
+      onTap: _recorrido,
+      //los botones deben ser mayores o igual a >=2
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.videogame_asset), title: Text("Juegos")),
+        BottomNavigationBarItem(icon: Icon(Icons.local_movies), title: Text("Películas"),),
+        BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text("Apps")),
+        BottomNavigationBarItem(icon: Icon(Icons.book),title: Text('Libros'),
+        ),
+      ],
+    );
+  }
+
+  Widget _drawern() {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.only(bottom: 12),
         children: <Widget>[
           UserAccountsDrawerHeader(
-            //Cuenta Principal
+            //definimos las cuentas
+            accountEmail: Text("nicolepgc91@gmail.com"),
+            accountName: Text("Nicole Garcia"),
+            //Cuenta Principal 
             currentAccountPicture: CircleAvatar(
-              child: Text("G",style: TextStyle(fontSize: 40,color: Colors.white),),
+              child: Text("G",
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
               backgroundColor: Colors.green[800],
             ),
             //Cuentas otras
             otherAccountsPictures: <Widget>[
               CircleAvatar(
-                child: Text("P",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white)),
+                child: Text("P",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white)),
                 backgroundColor: Colors.purple,
               ),
               CircleAvatar(
-                child: Text("N",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                child: Text("N",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
                 backgroundColor: Colors.green[300],
               )
             ],
-
-            accountEmail: Text("nicolepgc91@gmail.com"),
-            accountName: Text("Nicole Garcia"),
           ),
-          //cuerpo del drawer
+          //cuerpo del drawer nos definimos un widget aparte donde mandamos (icono,nombretarjeta)
           _listacuerpo(Icons.apps, "Mis apps"),
           _listacuerpo(Icons.add_alert, "Notificaciones"),
           _listacuerpo(Icons.loop, "Suscripciones"),
           _listacuerpo(Icons.turned_in_not, "Lista de Deseos"),
-          Divider(color: Colors.black54,),
+          //dibuja una linea horizontal
+          Divider(color: Colors.black54, ),
           _listacuerpo(Icons.person_outline, "Cuenta"),
           _listacuerpo(Icons.credit_card, "Formas de Pago"),
           _listacuerpo(Icons.security, "Play Protectet"),
@@ -67,33 +131,43 @@ class _DrawermState extends State<Drawerm> {
           _listaotro("Ayuda y Comentarios"),
           _listaotro("Guia para Padres"),
           _listaotro("Acerca de Google Play")
-          
-          
-          
         ],
       ),
     );
   }
 
   Widget _listacuerpo(IconData icon, String nombrec) {
-    return Container(
-      //color: Colors.redAccent,
-      width: double.infinity,
-      padding: EdgeInsets.only(top: 12,left: 14,bottom: 10),
+    return InkWell(
+        onTap: () {},
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(top: 12, left: 14, bottom: 10),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-        Icon(icon,size: 25,color: Colors.black54,),
-        SizedBox(width: 25,),
-        Expanded(child: Text(nombrec,style: TextStyle(fontSize: 15,color: Colors.black87),))],
-      ),
-    );
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 25,
+                color: Colors.black54,
+              ),
+              SizedBox(
+                width: 25,
+              ),
+              Expanded(
+                  child: Text(
+                nombrec,
+                style: TextStyle(fontSize: 15, color: Colors.black87),
+              ))
+            ],
+          ),
+        ));
   }
-  Widget _listaotro(String nombrecc){
+
+  Widget _listaotro(String nombrecc) {
     return Container(
-      padding: EdgeInsets.only(left: 14, bottom: 14,top: 12),
-      child:Text(nombrecc,style: TextStyle(fontSize: 15,color: Colors.black87),)
-    );
+        padding: EdgeInsets.only(left: 14, bottom: 14, top: 12),
+        child:
+        Text( nombrecc,
+          style: TextStyle(fontSize: 15, color: Colors.black87),
+    ));
   }
 }
